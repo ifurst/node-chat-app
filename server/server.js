df3968279382dc3d6dbbc9b10fa37a1657a2c924
Vinod Gnaniah <ifurst@outlook.com>
2018-02-07 14:46:58 +0530

    socket to send receive chat

--- server/server.js
+++ server/server.js
@@ -1,23 +1,53 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
-const publicPath = path.join(__dirname, '../public');
-console.log(publicPath);
-
-const app = express();
+const socketIO = require('socket.io');
 
 var port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 //Middle ware
 app.use(express.static(publicPath));
 
-//Public get/post request
-app.get('/', (q, r) => {
-    r.render('index.html');
+io.on('connection', (socket) => {
+    console.log('New Connection from client');
+
+    socket.on('createMessage', (m) => {
+        console.log(m);
+    });
+
+    socket.emit('newMessage', {
+        from: 'Vinod',
+        text: 'Hello there',
+        createdAt: 12234536
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected by client');
+    })
 });
+
+// io.on('connection', function (socket) {
+//     socket.emit('news', { hello: 'world' });
+//     socket.on('my other event', function (data) {
+//       console.log(data);
+//     });
+//   });
+
+console.log(publicPath);
+
+
+//Public get/post request
+// app.get('/', (q, r) => {
+//     r.render('index.html');
+// });
 
 
 //listen
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App up and running at ${port}`);
 })
